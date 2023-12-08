0e2d0102e598a9052a9c385a8f794469deb2291f
aas mohammad <aasmohd7071@gmail.com>
2023-12-08 17:00:03 +0530

    make light theme for all devices

--- app/src/main/java/com/aas/newspract/SplashActivity.kt
+++ app/src/main/java/com/aas/newspract/SplashActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
 
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // to avoid dark theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
